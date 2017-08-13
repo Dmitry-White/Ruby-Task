4bb1db287f982d5ab433fc003a38a381737f5d69
Dmitry White <dmitry.b.white@gmail.com>
2017-08-14 01:04:41 +0300

    SSL Certification problem

--- script.rb
+++ script.rb
@@ -1,10 +1,15 @@
 require 'curb'
 require 'nokogiri'
 
+http = Curl::Easy.new("http://www.petsonic.com/es/perros/snacks-y-huesos-perro") do |curl|
+  curl.verbose = true
+  curl.ssl_verify_host = false
+end
+http.perform
+puts http.body_str
 
-http = Curl.get("http://www.petsonic.com/es/perros/snacks-y-huesos-perro")
 
-puts http.bidy_str
+
 
 #html = Nokogiri::HTML(http.body_str)
 
