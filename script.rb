292bb972224ef5dc355a3051f41d454b7bc70fce
Dmitry White <dmitry.b.white@gmail.com>
2017-08-14 02:59:37 +0300

    Read titles

--- script.rb
+++ script.rb
@@ -10,8 +10,19 @@ end
 
 html = Nokogiri::HTML(url.body_str)
 
-puts html
+titles = Array.new
 
-File.open("script_output.html", "w") do |file|
-  file << html
+html.xpath('//div/h5/a/text()').each do |title|
+  titles << title
 end
+
+puts titles
+
+
+File.open("titles_output.txt", "wb") do |file|
+  (0..titles.length-1).each do |index|
+    file.puts(titles[index])
+  end
+end
+
+
