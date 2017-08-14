45b741bd0213a869f7a43f4ec461b92d176df96f
Dmitry White <dmitry.b.white@gmail.com>
2017-08-14 14:43:34 +0300

    Add ARGV feature

--- script.rb
+++ script.rb
@@ -2,20 +2,31 @@ require 'nokogiri'
 require 'curb'
 require 'csv'
 
-address = "http://www.petsonic.com/es/perros/snacks-y-huesos-perro"
-url_list = Array.new
 
-#---------------- Functions Definitions ----------------
+#------------------ Reading Input -------------------------------
+ARGC = ARGV.length
+if ARGC < 2 or ARGC > 2
+  puts "Usage: ruby <input_file> <output_file> <URL>"
+  abort
+end
+
+output = ARGV[0]
+address = ARGV[1]
+#----------------------------------------------------------------
+
+
+#---------------- Functions Definitions -------------------------
 def get_html(link)
   url = Curl.get(link) do |curl|
-    curl.verbose = true
+    #curl.verbose = true
     curl.follow_location = true
     curl.ssl_verify_peer = false
   end
   html = Nokogiri::HTML(url.body_str)
   html
 end
-# ------------------------------------------------------
+# ---------------------------------------------------------------
+
 
 html = get_html(address)
 url = html.xpath('//link[@rel="canonical"]/@href').to_s
@@ -24,51 +35,71 @@ url = html.xpath('//link[@rel="canonical"]/@href').to_s
 another_page = true
 page_num = 1
 
-#------------------- Scraping with Xpath ----------------
+#------------------- Scraping with Xpath -------------------------
+url_list = Array.new
 while another_page == true
-  urls = Array.new
 
+  urls = Array.new
   html.xpath('//div/h5/a/@href').each do |url|
     urls << url
   end
-
-  #puts urls
   url_list << urls
 
-  #-------------------------------------------------------
-
-
-  #-------------------- Checking for the next page -------
+  #-------------------- Checking for the next page --------------------------
   disabled_right_button = html.xpath('boolean(//li[@id="pagination_next_bottom" and not(@class="disabled pagination_next")])')
-
   if disabled_right_button
     html = get_html(url + "?p=#{page_num+1}")
   else
     another_page = false
-    puts "HEEEEEEEEEEEEY"
   end
+  #--------------------------------------------------------------------------
 
+
+  puts "Added product links from page " + page_num.to_s
   page_num += 1
 end
-#-------------------------------------------------------
+#-----------------------------------------------------------------
 
-File.open("titles_output.txt", "wb") do |file|
-  (0..url_list.length-1).each do |index|
+puts "Pages are listed. Beginning scrapping sequence."
+
+len = url_list.length
+
+#------------------- Write file with list of page links ----------
+File.open("url_output.txt", "wb") do |file|
+  (0..len-1).each do |index|
     file.puts(url_list[index])
   end
 end
-
-puts url_list
-
+#-----------------------------------------------------------------
 
 
+#-------------------- Search for products details ------------------
+url_list = File.readlines('titles_output.txt')
+product_num = 1
 
-#puts html
-=begin
-CSV.open("titles.csv", "wb") do |row|
-  (0..titles.length - 1).each do |index|
-    row << ["Title",titles[index]]
+url_list.each do |link|
+  link = link.strip
+  product_html = get_html(link)
+
+  product_name = product_html.xpath('//div[@class="product-name"]/h1/text()')
+  product_weights = product_html.xpath('//li/span[@class="attribute_name"]/text()')
+  product_prices = product_html.xpath('//li/span[@class="attribute_price"]/text()')
+  product_images = product_html.xpath('//a[@data-fancybox-group="other-views"]/@href')
+
+
+  #--------------------------- Write CSV file ---------------------------------------
+  CSV.open(output, 'a') do |row|
+    (0..product_weights.length-1).each do |index|
+      row << ['Name: ', product_name.to_s.strip + ' - '  + product_weights[index].to_s]
+      row << ['Price: ', product_prices[index].to_str]
+      row << ['Image: ', product_images[index]]
+      row << ['-----------------------------------------------------------------------------']
+    end
   end
-end
-=end
+  #----------------------------------------------------------------------------------
 
+
+  puts "Description of product " + product_num.to_s + " added to .csv file."
+  product_num += 1
+end
+#-------------------------------------------------------------------------
