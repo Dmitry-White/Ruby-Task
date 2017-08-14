7ce8da66882695add69726650c09522a1b59d642
Dmitry White <dmitry.b.white@gmail.com>
2017-08-14 16:24:52 +0300

    Final changes

--- script.rb
+++ script.rb
@@ -12,10 +12,12 @@ end
 
 output = ARGV[0]
 address = ARGV[1]
+
+puts 'Input received. Getting source html file.'
 #----------------------------------------------------------------
 
 
-#---------------- Functions Definitions -------------------------
+#---------------- Nokogiri Parse Function  -------------------------
 def get_html(link)
   url = Curl.get(link) do |curl|
     #curl.verbose = true
@@ -31,21 +33,26 @@ end
 html = get_html(address)
 url = html.xpath('//link[@rel="canonical"]/@href').to_s
 
+puts 'Source html file received.'
 
 another_page = true
 page_num = 1
+urls_num = 0
 
-#------------------- Scraping with Xpath -------------------------
+puts 'Initiating page URL listing sequence.'
+#------------------- Scrap with Xpath -------------------------
 url_list = Array.new
 while another_page == true
 
+  numb = 0
   urls = Array.new
   html.xpath('//div/h5/a/@href').each do |url|
     urls << url
+    numb += 1
   end
+  urls_num += numb
   url_list << urls
-
-  #-------------------- Checking for the next page --------------------------
+  #-------------------- Check for the next page --------------------------
   disabled_right_button = html.xpath('boolean(//li[@id="pagination_next_bottom" and not(@class="disabled pagination_next")])')
   if disabled_right_button
     html = get_html(url + "?p=#{page_num+1}")
@@ -55,26 +62,30 @@ while another_page == true
   #--------------------------------------------------------------------------
 
 
-  puts "Added product links from page " + page_num.to_s
+  puts "      Added product links from page " + page_num.to_s
   page_num += 1
 end
 #-----------------------------------------------------------------
-
-puts "Pages are listed. Beginning scrapping sequence."
-
 len = url_list.length
 
+puts 'Pages are listed. Total number of links: ' + urls_num.to_s
+
+
+
 #------------------- Write file with list of page links ----------
+link_num=0
 File.open("url_output.txt", "wb") do |file|
   (0..len-1).each do |index|
     file.puts(url_list[index])
+    link_num += 1
   end
 end
 #-----------------------------------------------------------------
 
+puts 'Initiating scrapping sequence.'
 
 #-------------------- Search for products details ------------------
-url_list = File.readlines('titles_output.txt')
+url_list = File.readlines('url_output.txt')
 product_num = 1
 
 url_list.each do |link|
@@ -99,7 +110,11 @@ url_list.each do |link|
   #----------------------------------------------------------------------------------
 
 
-  puts "Description of product " + product_num.to_s + " added to .csv file."
+  puts "      Description of product " + product_num.to_s + " added to .csv file."
   product_num += 1
+  if product_num == 50
+    break
+  end
 end
+puts 'Scraping complete. Refer to ' + ARGV[0] + ' file for script output.'
 #-------------------------------------------------------------------------
